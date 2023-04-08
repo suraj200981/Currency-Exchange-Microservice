60c6f8b68c84f86cfd2d1bc8ae64c32044dc1605
suraj <suraj200981@yahoo.co.uk>
2023-04-08 11:39:02 +0100

    seeding test data with import.sql in resources directory

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into currency_exchange (id, conversion_multiple, currency_from, currency_to, environment) values (1000, 50, 'USD', 'INR', '8000');
+insert into currency_exchange (id, conversion_multiple, currency_from, currency_to, environment) values (1001, 100, 'GBP', 'INR', '8001');
+insert into currency_exchange (id, conversion_multiple, currency_from, currency_to, environment) values (1002, 150, 'EUR', 'INR', '8002');
+insert into currency_exchange (id, conversion_multiple, currency_from, currency_to, environment) values (1003, 200, 'AUD', 'INR', '8003');
+insert into currency_exchange (id, conversion_multiple, currency_from, currency_to, environment) values (1004, 250, 'CAD', 'INR', '8004');
